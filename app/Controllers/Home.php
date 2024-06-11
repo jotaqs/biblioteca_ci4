f654ba6e0cb31ec2192376f7a84a93e01d2be924
jotaveqs <joaovittorss2007@gmail.com>
2024-06-11 19:04:07 -0300

    first commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Home extends BaseController
+{
+    public function index(){
+        echo view('_partials/header');
+        echo view('home/index.php');
+        echo view('_partials/footer');
+    }
+}
---
+++ app/Views/home/index.php
@@ -0,0 +1,31 @@
+<head>
+<style>
+        body, html {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .conteudo-centralizado {
+            text-align: center;
+        }
+    </style>
+</head>
+<div class="conteudo-centralizado">
+<div class="container p-4">
+    <center>
+    <h2>Biblioteca da EEEP Walter Ramos de Araujo</h2>
+    <br>
+    <a class="btn btn-dark" href="/index.php/Aluno/index" role="button">Aluno</a>
+    <a class="btn btn-dark" href="/index.php/Autor/index" role="button">Autor</a>
+    <a class="btn btn-dark" href="/index.php/Editora/index" role="button">Editora</a>
+    <a class="btn btn-dark" href="/index.php/Emprestimo/index" role="button">Emprestimo</a>
+    <a class="btn btn-dark" href="/index.php/Livro/index" role="button">Livro</a>
+    <a class="btn btn-dark" href="/index.php/Obra/index" role="button">Obra</a>
+    <a class="btn btn-dark" href="/index.php/Usuario/index" role="button">Usuario</a>
+
+    </div>
+</center>
+</div>   
+    </div>
